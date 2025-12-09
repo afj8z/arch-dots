485cd114b9cb0fb5c01c1b9cadb155440852bc36
afj8z <aidflem@gmail.com>
2025-12-09 17:04:47 +0100

    theme templating update

---
+++ wal/.config/wal/templates/shell-theme.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Based on your "special" colors
+export COLOR_BG="{background}"       # myblack / background
+export COLOR_FG="{foreground}"       # myfront / foreground
+export COLOR_ACCENT="{myaccent}"   # myaccent
---
+++ zsh/.config/zsh/shell-theme.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Based on your "special" colors
+export COLOR_BG="#111012"       # myblack / background
+export COLOR_FG="#DBD0C6"       # myfront / foreground
+export COLOR_ACCENT="#8A7FA3"   # myaccent
